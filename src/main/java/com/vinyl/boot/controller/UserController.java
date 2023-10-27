5c5d09a917f5c2885675ca0999d9ec26ed7734ef
CHANHAN <130114269+chanani@users.noreply.github.com>
2023-10-28 00:15:15 +0900

    login request

--- src/main/java/com/vinyl/boot/controller/UserController.java
+++ src/main/java/com/vinyl/boot/controller/UserController.java
@@ -6,26 +6,41 @@ import com.vinyl.boot.user.service.UserMapper;
 import com.vinyl.boot.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequestMapping("/user")
+@SessionAttributes("username")
 public class UserController {
 
     @Autowired
     @Qualifier("userService")
     private UserService userService;
+
+    @PostMapping("/login")
+    public String login(UserVO vo,
+                        RedirectAttributes ra, SessionStatus sessionStatus){
+        if (vo.getUsername() == null || vo.getPassword() == null){
+            String msg = "아이디와 비밀번호는 공백일 수 없습니다.";
+            ra.addFlashAttribute("msg", msg);
+            return "redirect:/";
+        }
+        System.out.println(sessionStatus.toString());
+
+        return "redirect:/";
+    }
+
 
     @PostMapping("/joinForm")
     public String joinForm(@Valid @ModelAttribute("vo") ValidVO vo, Errors errors,
